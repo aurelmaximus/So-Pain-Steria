2fd270fc86411edf4900e6bec0389a363570d39c
Cedric <devil-ced@hotmail.com>
2023-01-11 11:15:13 +0100

    Ajout IDAOCompte et IDAOCommande

---
+++ Boulangerie-back/src/main/java/dao/IDAOCommande.java
@@ -0,0 +1,14 @@
+package dao;
+
+import java.util.List;
+
+import model.Commande;
+import model.Compte;
+
+
+
+public interface IDAOCommande extends IDAO<Compte,Integer> {
+
+	public List<Commande> findAllByClient(Integer idClient);
+	
+}
