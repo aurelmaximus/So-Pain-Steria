4d0a9b0029400ce7b9cf4f1cc114e3947ece2a2f
Cedric <devil-ced@hotmail.com>
2023-01-27 17:42:45 +0100

    test.java

--- Boulangerie-back/src/test/java/test/test.java
+++ Boulangerie-back/src/test/java/test/test.java
@@ -1,11 +1,20 @@
 package test;
 
-
+import boulangerie.context.Application;
+import repository.IIngredientRepository;
+import repository.IProduitRepository;
 
 public class test {
 
 	public static void main(String[] args) {
 		
+		//ICommandeRepository commandeRepo = Application.getInstance()
+		//ICompteRepository compteRepo = Application.getInstance()
+		
+		IProduitRepository produitRepo = Application.getInstance().getProduitRepo();
+		IIngredientRepository ingredientRepo = Application.getInstance().getIngredientRepo();
+		
+
 	
 		
 	}
