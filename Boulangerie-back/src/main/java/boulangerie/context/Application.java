acd16776d87b0f7fb0ca2c95752d1d0bbca28ca6
farahba <farah.ben.aissa@outlook.com>
2023-01-27 11:57:47 +0100

    Auto stash before merge of "develop" and "origin/develop"

---
+++ Boulangerie-back/src/main/java/boulangerie/context/Application.java
@@ -0,0 +1,34 @@
+package boulangerie.context;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+
+
+public class Application {
+
+	private static Application instance = null;
+
+	private final EntityManagerFactory emf = Persistence.createEntityManagerFactory("quest");
+	//private DAOCommande daoCommande = new DAOCommande();
+	//private DAOCompte daoCompte = new DAOCompte();
+	//private DAOIngredient daoIngredient = new DAOIngredient();
+	//private DAOProduit daoProduit = new DAOProduit();
+	//private static Application instance;
+	
+	
+	
+	private Application() {
+	}
+
+	public static Application getInstance() {
+		if (instance == null) {
+			instance = new Application();
+		}
+
+		return instance;
+	}
+
+	}
+
+
