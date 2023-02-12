e576181eb1849afb05ab95ef1e38a4921ad666ce
Cedric <devil-ced@hotmail.com>
2023-02-12 18:38:50 +0100

    (commandeRepo et ICompteRepo en // pour éviter les erreurs)

--- Boulangerie-back/src/main/java/repository/jpa/CommandeRepository.java
+++ Boulangerie-back/src/main/java/repository/jpa/CommandeRepository.java
@@ -1,120 +1,120 @@
-package repository.jpa;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.EntityTransaction;
-import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
-import javax.transaction.Transactional;
-
-import org.springframework.stereotype.Repository;
-
-import boulangerie.context.Application;
-import boulangerie.model.Client;
-import boulangerie.model.Commande;
-import boulangerie.model.EtatCommande;
-import boulangerie.repository.CommandeRepository;
-
-@Repository
-public class CommandeRepository implements CommandeService {
-
-	@PersistenceContext
-	private EntityManager em;
-
-	@Override
-	public List<Commande> findAll() {
-
-		List<Commande> commandes = new ArrayList<>();
-
-		TypedQuery<Commande> query = em.createQuery("select c from Commande c", Commande.class);
-		commandes = query.getResultList();
-
-		return commandes;
-	}
-
-	@Override
-	public Commande findById(Integer id) {
-
-		Commande commande = null;
-
-		commande = em.find(Commande.class, id);
-
-		return commande;
-	}
-
-	@Override
-	@Transactional
-	public Commande save(Commande commande) {
-
-		commande = em.merge(commande);
-
-		return commande;
-	}
-
-	@Override
-	@Transactional
-	public void deleteById(Integer numero) {
-
-		TypedQuery<Commande> query = em.createQuery("delete from Commande c where c.numero = ?1", Commande.class);
-		query.setParameter(1, numero);
-
-		query.executeUpdate();
-
-	}
-
-	@Override
-	@Transactional
-	public void delete(Commande commande) {
-
-		em.remove(em.merge(commande));
-
-	}
-
-	@Override
-	public List<Commande> findAllByClient(Client client) {
-
-		List<Commande> commandes = new ArrayList<>();
-
-		TypedQuery<Commande> query = em.createQuery("select c from Commande c where c.client = :cl", Commande.class);
-
-		query.setParameter("cl", client);
-		commandes = query.getResultList();
-
-		return commandes;
-	}
-
-
-	@Override
-	public List<Commande> findAllByEtatCommande(EtatCommande etatC) {
-
-		List<Commande> commandes = new ArrayList<>();
-
-		TypedQuery<Commande> query = em.createQuery("select c from Commande c where  c.etatCommande = :et", Commande.class);
-
-		query.setParameter("et", etatC);
-		commandes = query.getResultList();
-
-		return commandes;
-	}
-
-	@Override
-	public List<Commande> findAllByClientAndEtatCommande(Client client, EtatCommande etatC) {
-
-		List<Commande> commandes = new ArrayList<>();
-
-		TypedQuery<Commande> query = em.createQuery("select c from Commande c where c.client = :cl and c.etatCommande = :et", Commande.class);
-		query.setParameter("cl", client);
-		query.setParameter("et", etatC);
-
-		commandes = query.getResultList();
-
-		return commandes;
-	}
-
-
-
-
-
-}
+//package repository.jpa;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//import javax.persistence.EntityManager;
+//import javax.persistence.EntityTransaction;
+//import javax.persistence.PersistenceContext;
+//import javax.persistence.TypedQuery;
+//import javax.transaction.Transactional;
+//
+//import org.springframework.stereotype.Repository;
+//
+//import boulangerie.context.Application;
+//import boulangerie.model.Client;
+//import boulangerie.model.Commande;
+//import boulangerie.model.EtatCommande;
+//import boulangerie.repository.CommandeRepository;
+//
+//@Repository
+//public class CommandeRepository implements CommandeService {
+//
+//	@PersistenceContext
+//	private EntityManager em;
+//
+//	@Override
+//	public List<Commande> findAll() {
+//
+//		List<Commande> commandes = new ArrayList<>();
+//
+//		TypedQuery<Commande> query = em.createQuery("select c from Commande c", Commande.class);
+//		commandes = query.getResultList();
+//
+//		return commandes;
+//	}
+//
+//	@Override
+//	public Commande findById(Integer id) {
+//
+//		Commande commande = null;
+//
+//		commande = em.find(Commande.class, id);
+//
+//		return commande;
+//	}
+//
+//	@Override
+//	@Transactional
+//	public Commande save(Commande commande) {
+//
+//		commande = em.merge(commande);
+//
+//		return commande;
+//	}
+//
+//	@Override
+//	@Transactional
+//	public void deleteById(Integer numero) {
+//
+//		TypedQuery<Commande> query = em.createQuery("delete from Commande c where c.numero = ?1", Commande.class);
+//		query.setParameter(1, numero);
+//
+//		query.executeUpdate();
+//
+//	}
+//
+//	@Override
+//	@Transactional
+//	public void delete(Commande commande) {
+//
+//		em.remove(em.merge(commande));
+//
+//	}
+//
+//	@Override
+//	public List<Commande> findAllByClient(Client client) {
+//
+//		List<Commande> commandes = new ArrayList<>();
+//
+//		TypedQuery<Commande> query = em.createQuery("select c from Commande c where c.client = :cl", Commande.class);
+//
+//		query.setParameter("cl", client);
+//		commandes = query.getResultList();
+//
+//		return commandes;
+//	}
+//
+//
+//	@Override
+//	public List<Commande> findAllByEtatCommande(EtatCommande etatC) {
+//
+//		List<Commande> commandes = new ArrayList<>();
+//
+//		TypedQuery<Commande> query = em.createQuery("select c from Commande c where  c.etatCommande = :et", Commande.class);
+//
+//		query.setParameter("et", etatC);
+//		commandes = query.getResultList();
+//
+//		return commandes;
+//	}
+//
+//	@Override
+//	public List<Commande> findAllByClientAndEtatCommande(Client client, EtatCommande etatC) {
+//
+//		List<Commande> commandes = new ArrayList<>();
+//
+//		TypedQuery<Commande> query = em.createQuery("select c from Commande c where c.client = :cl and c.etatCommande = :et", Commande.class);
+//		query.setParameter("cl", client);
+//		query.setParameter("et", etatC);
+//
+//		commandes = query.getResultList();
+//
+//		return commandes;
+//	}
+//
+//
+//
+//
+//
+//}
