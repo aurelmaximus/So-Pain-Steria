9f455478bfba6492b44f0d19ff474a2efea9e11b
Cedric <devil-ced@hotmail.com>
2023-02-13 10:45:01 +0100

    Ingredient Ok

--- Boulangerie-back/src/main/java/boulangerie/repository/IngredientRepository.java
+++ Boulangerie-back/src/main/java/boulangerie/repository/IngredientRepository.java
@@ -4,15 +4,18 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import boulangerie.model.Ingredient;
 
 
 public interface IngredientRepository extends JpaRepository<Ingredient,Integer> {
-	
-//	@Query("select i ingredient i left join fetch i.lignesIngredient li where li.produit.libelle=:libelleProduit")
-//	List<Ingredient> findByLignesingredientProduitLibelle (String libelleProduit);
-	
-	@Query("select i from Ingredient i left join fetch i.ligneIngredients where i.libelle=:libelle")
+		
+	@Query("select i from Ingredient i left join fetch i.lignesIngredient where i.libelle=:libelle")
 	List<Ingredient> findAllByLibelleWithLigneIngredients(String libelle);
+	
+
+	@Query("select i from Ingredient i left join fetch i.lignesIngredient li where li.produit.libelle=:libelleProduit")
+	List<Ingredient> findByLignesingredientProduitLibelle (@Param("libelleProduit")String libelleProduit);
+	
 }
