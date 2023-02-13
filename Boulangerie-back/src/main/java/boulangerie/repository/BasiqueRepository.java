2c435c549e45587948cfb31fcd7106df2650af94
Aurel <aurelien.bulme@gmail.com>
2023-02-13 18:35:27 +0100

    Query - Elabore et Basique

--- Boulangerie-back/src/main/java/boulangerie/repository/BasiqueRepository.java
+++ Boulangerie-back/src/main/java/boulangerie/repository/BasiqueRepository.java
@@ -1,12 +1,18 @@
 package boulangerie.repository;
 
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import boulangerie.model.Basique;
 
 
-//A voir si la query est bonne
 public interface BasiqueRepository extends JpaRepository<Basique,Integer>{
+	
+	@Query("select b from Basique b left join fetch b.ligneIngredients where b.libelle=:libelle")
+	List<Basique> findAllByLibelleWithLigneIngredients(String libelle);
 	
 
 }
--- Boulangerie-back/src/main/java/boulangerie/repository/ElaboreRepository.java
+++ Boulangerie-back/src/main/java/boulangerie/repository/ElaboreRepository.java
@@ -1,11 +1,17 @@
 package boulangerie.repository;
 
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import boulangerie.model.Elabore;
 
-//A voir si la query est bonne
 public interface ElaboreRepository extends JpaRepository<Elabore,Integer>{
+	
+	@Query("select  e from Elabore e left join fetch e.ligneIngredients where e.libelle=:libelle")
+	List<Elabore> findAllByLibelleWithLigneIngredients(String libelle);
 	
 
 }
