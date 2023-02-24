f60792d153141b0e95b4c59ba2fff3c544776bf7
YT <talmat.youssef@gmail.com>
2023-02-24 12:17:47 +0100

    ajout de model.ts

--- Angular/sopain-angular/src/app/app.module.ts
+++ Angular/sopain-angular/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FooterComponent } from './footer/footer.component';
+import { InscriptionComponent } from './inscription/inscription.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    FooterComponent
+    FooterComponent,
+    InscriptionComponent
   ],
   imports: [
     BrowserModule,
