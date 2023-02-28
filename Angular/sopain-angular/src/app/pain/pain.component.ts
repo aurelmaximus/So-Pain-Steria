8b3e4f7bb25546d15c77e905a0440af7346b5095
farah <farah.ben.aissa@outlook.com>
2023-02-28 15:45:49 +0100

    vien+pain dynamiques

--- Angular/sopain-angular/src/app/pain/pain.component.ts
+++ Angular/sopain-angular/src/app/pain/pain.component.ts
@@ -9,11 +9,30 @@ import { Basique } from '../model';
   styleUrls: ['./pain.component.css']
 })
 export class PainComponent {
-  libelle:string;
-  prix:number;
+  pains: Array<Basique> = new Array<Basique>();
+  pain: Basique = new Basique();
 
-  constructor(private basiqueService: BasiqueService)  { }
+  constructor(private basiqueService: BasiqueService)  { 
+  this.findPains();
+  }
+
+  list(): Array<Basique> {
+    return this.basiqueService.findAll();
+  }
+
+  findPains(): void {
+    this.basiqueService.findByCat('pain').subscribe(resp => {
+      this.pains = resp;
+    });
+  }
+
+  findPainByLib(lib: string) {
+    this.basiqueService.findByLib(lib).subscribe(response => {
+      this.pain = response;
+    });
+  }
 
 
-
+   
 }
+
