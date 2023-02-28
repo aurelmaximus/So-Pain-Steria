5ce2dbb6276b59106f5b8b2f5791cff5c3fa3534
Cedric <devil-ced@hotmail.com>
2023-02-28 11:38:33 +0100

    FindByCat BasiqueService

--- Angular/sopain-angular/src/app/basique.service.ts
+++ Angular/sopain-angular/src/app/basique.service.ts
@@ -10,6 +10,7 @@ export class BasiqueService {
 
   basiques: Array<Basique> = new Array<Basique>();
 
+
   constructor(private http: HttpClient) {
     this.load();
   }
@@ -23,14 +24,16 @@ export class BasiqueService {
     return this.http.get<Basique>("http://localhost:8888/basique/" + id);
   }
 
+  findByCat(cat: string): Observable<Array<Basique>> {
+    return this.http.get<Array<Basique>>("http://localhost:8888/basique/categorie" + cat);
+  }
+
+  create(basique: Basique): void {
+    this.http.post<Basique>("http://localhost:8888/basique", basique).subscribe(resp => {
  findByLib(libelle: string): Observable<Basique> {
     return this.http.get<Basique>("http://localhost:8888/basique/libelle" + libelle);
   }
 
-  create(basique: Basique): void {
-    this.http.post<Basique>("http://localhost:8888/basique/libelle", basique).subscribe(resp => {
-
- 
       this.load();
     });
   }
--- Angular/sopain-angular/src/app/patisserie/patisserie.component.ts
+++ Angular/sopain-angular/src/app/patisserie/patisserie.component.ts
@@ -9,12 +9,29 @@ import { Basique } from '../model';
 })
 export class PatisserieComponent {
 
-  constructor(private basiqueServ: BasiqueService) {
+  patisseries: Array<Basique> = new Array<Basique>();
+  pat: Basique = new Basique();
 
+  constructor(private basiqueServ: BasiqueService) {
+    this.findPatisseries();
   }
 
   list(): Array<Basique> {
     return this.basiqueServ.findAll();
   }
+
+  findPatisseries(): void {
+    this.basiqueServ.findByCat('gateau').subscribe(resp => {
+      this.patisseries = resp;
+    });
+  }
+
+  findPatByLib(lib: string) {
+    this.basiqueServ.findByLib(lib).subscribe(response => {
+      this.pat = response;
+    });
+  }
+
+
    
 }
