8c2983f24bc4af972f4666567dc9aadfb0dea0b9
farah <farah.ben.aissa@outlook.com>
2023-03-01 15:53:46 +0100

    ajoutRoutePanier

--- Angular/sopain-angular/src/app/panier/panier.component.ts
+++ Angular/sopain-angular/src/app/panier/panier.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth/auth.service';
 import { ClientHttpService } from '../client/client-http.service';
-import { Basique, Client, Produit } from '../model';
+import { CommandeHttpService } from '../commande/commande-http.service';
+import { CompteclientHttpService } from '../compteclient/compteclient-http.service';
+import { Basique, Client, Commande, Produit } from '../model';
 import { PanierService } from './panier.service';
 
 @Component({
@@ -20,6 +22,18 @@ export class PanierComponent {
 
   client: Client = new Client();
   articles: Array<Produit> = new Array<Produit>();
+  compteclientService: any;
+  commandeService: any;
+  
+  
+
+  clientconnecte(): Client {
+    return this.compteclientService.currentclient;
+  }
+  listCommandesEnCours(): Array<Commande> {
+    const client = this.clientconnecte();
+    return client?.id ? this.commandeService.findAllclientEncours(client) : [];
+  }
 
   toggleText() {
     this.showText = !this.showText;
@@ -53,12 +67,11 @@ export class PanierComponent {
 
   pay() {
       //ajouter articles aux commandes
-      window.location.href = "http://localhost:4200/client"; }
-
+      this.router.navigate(['/client']);}
 
     validate() {
       //ajouter articles aux commandes
-      window.location.href = "http://localhost:4200/client";  }
+      this.router.navigate(['/client']);}
 
 }
 
